6c218a01e8f64395b1ebc13acffbce14d5647512
DarioGuastella <jimmyguastella@gmail.com>
2023-11-14 20:18:00 +0100

    feat: aggiunge bottoni al posto dei link "like"

--- main.js
+++ main.js
@@ -120,10 +120,10 @@ posts.forEach((element) => {
     <div class="post__footer">
         <div class="likes js-likes">
             <div class="likes__cta">
-                <a class="like-button  js-like-button" href="#" data-postid="1">
+                <button class="like-button  js-like-button" data-postid="1">
                     <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
                     <span class="like-button__label">Mi Piace</span>
-                </a>
+                </button>
             </div>
             <div class="likes__counter">
                 Piace a <b id="like-counter-1" class="js-likes-counter">${element.likes}</b> persone
